c6b34b760ecc1f7028f94a879c824431649150a4
root <root@DESKTOP-KM7QQE6>
2024-11-03 23:34:15 -0300

    add ✅  exercises solved

--- Exercícios/NumeracaoRomana.c
+++ Exercícios/NumeracaoRomana.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    
+void conversorDecRom(int num){
+    int valores[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    char *romanos[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    for (int i = 0; i < 13; i++) {
+        while (num >= valores[i]) {
+            printf("%s", romanos[i]);
+            num -= valores[i];
+        }
+    }
+    printf("\n");     
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    conversorDecRom(n);    
     return 0;
 }
