6dc0426da48c8cf1a72bf161986e0c47c33cc824
root <root@DESKTOP-KM7QQE6>
2024-11-04 00:41:25 -0300

    add exercises

--- Exercícios/OMaior.c
+++ Exercícios/OMaior.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     int a,b,c,maiorAB, maiorFinal;
     scanf("%d %d %d", &a, &b, &c);
 
-    maiorAB = a;
+    maiorAB = ((a+b+abs(a-b))/2);
+    maiorFinal = ((maiorAB+c+abs(maiorAB-c))/2);
 
+    printf("%d eh o maior\n", maiorFinal);
     return 0;
 }
